7d38c53f5501d7c2a160eec63cee879ee96fe664
Caio de Alcantara Santos <95832094+caio-alcantara@users.noreply.github.com>
2024-06-27 14:03:04 -0300

    substitui URL por variavel de ambiente nos testes do cat_service

--- test_cat_service.py
+++ test_cat_service.py
@@ -1,6 +1,10 @@
 from cat_service import CatService
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def test_get_cat_image_returns_url():
     cat_service = CatService()
-    assert cat_service.get_content().content == 'https://cataas.com/cat'
+    assert cat_service.get_content().content == os.getenv("CAT_IMG_URL")
     assert cat_service.get_content().content_type == 'media'
